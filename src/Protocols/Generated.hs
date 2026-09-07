23971dd7f738f4ff26f9f6aef54aa1ed43088946
Crdr0122 <161446951+Crdr0122@users.noreply.github.com>
2026-09-07 11:48:45 +0900

    Add waywire dependency

---
+++ src/Protocols/Generated.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+
+module Protocols.Generated where
+
+import Wayland.Generated
+
+$(generateModules "protocols")
+
